211a2680c45488262013cb0e01f785354b33e7b6
Parth-11 <parth12.mgg@gmail.com>
2025-10-25 00:12:56 +0530

    added pgx package to handle postgres

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
 	router := gin.Default()
+
+	con, err := pgx.Connect(context.Background(), "")
+
+	if err != nil {
+		fmt.Println("Unable to connect to database", err)
+		os.Exit(1)
+	}
+	defer con.Close(context.Background())
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
