862b61d0b2964d83b2079ef4263aa1ebcb802ecb
Parth-11 <parth12.mgg@gmail.com>
2025-10-24 22:03:00 +0530

    setup a simple server  using gin-framework

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	if err := router.Run("8080"); err != nil {
+		fmt.Println("Failed to start server on the port 8080", err)
+	}
+}
